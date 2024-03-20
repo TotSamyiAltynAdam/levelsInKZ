5af2e54963517cdd6ab1369fe25dcdb0317ef5b3
Adina <adina.meiramkhanova@narxoz.kz>
2024-03-21 00:27:17 +0500

    delete a mui, and added personal styles for avoiding errors

--- src/pages/signin/SignIn.jsx
+++ src/pages/signin/SignIn.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { TextField, InputLabel, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 
 import { useAuthContext } from "../../context/AuthContextProvider";
+
 import { signIn } from "../../api/api";
 import Header from "../../components/header/Header";
 
@@ -15,26 +15,26 @@ const SignIn = () => {
   const handleSubmit = async (values) => {
     try {
       const response = await signIn(values);
+      // setToken(response.token);
       localStorage.setItem("token", response.token);
       navigate("/");
     } catch (error) {
       console.log(error);
     }
   };
-
   const validate = (values) => {
     const errors = {};
 
     if (!values.email) {
-      errors.email = "Required!";
+      errors.email = "Обязательное поля!";
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-      errors.email = "Invalid email address";
+      errors.email = "Неправильный email адрес";
     }
 
     if (!values.password) {
-      errors.password = "Required!";
+      errors.password = "Обязательное поля!";
     } else if (values.password.length < 6) {
-      errors.password = "Must be at least 6 characters!";
+      errors.password = "Минимум 6 символа для заполнения!";
     }
     return errors;
   };
@@ -46,132 +46,140 @@ const SignIn = () => {
     },
     validate,
     onSubmit: (values) => handleSubmit(values),
+    // onSubmit: values => console.log(JSON.stringify(values, null, 2))
   });
 
   return (
     <>
       <Header />
-      <div
+      <form
+        className="loginForm"
         style={{
           display: "flex",
           flexDirection: "column",
+          marginTop: "80px",
           alignItems: "center",
-          marginTop: "57px",
         }}
+        onSubmit={formik.handleSubmit}
       >
-        <h1>Sign In</h1>
-        <div
+        <h1 style={{ marginBottom: "18px" }}>Sign In</h1>
+        <input
+          className="loginForm-input"
+          id="email"
+          name="email"
+          type="text"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Email"
           style={{
-            marginBottom: "16px",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <InputLabel
-            htmlFor="email"
-            sx={{ marginBottom: "17px", color: "black", fontWeight: "700" }}
-          >
-            Email Address
-          </InputLabel>
-          <TextField
-            id="email"
-            name="email"
-            type="email"
-            variant="outlined"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.email && Boolean(formik.errors.email)}
-            helperText={formik.touched.email && formik.errors.email}
-            style={{ width: "550px" }}
-          />
-        </div>
-        <div
-          style={{
-            marginBottom: "16px",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <InputLabel
-            htmlFor="password"
-            sx={{ marginBottom: "17px", color: "black", fontWeight: "700" }}
-          >
-            Password{" "}
-            <span
-              style={{
-                color: "#E87070",
-                marginLeft: "320px",
-                cursor: "pointer",
-              }}
-            >
-              Forgot Password?
-            </span>
-          </InputLabel>
-          <TextField
-            id="password"
-            name="password"
-            type="password"
-            variant="outlined"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.password && Boolean(formik.errors.password)}
-            helperText={formik.touched.password && formik.errors.password}
-            style={{ width: "550px" }}
-          />
-        </div>
-        <Button
-          style={{
-            backgroundColor: "#163A4E",
-            padding: "15px",
-            color: "#ffffff",
             width: "550px",
             borderRadius: "15px",
-            marginTop: "12px",
-          }}
-          color="inherit"
-          onClick={formik.handleSubmit}
-        >
-          Sign In
-        </Button>
-        <p style={{ fontWeight: "500", marginTop: "15px" }}>
-          Don’t have an account?{" "}
-          <Link
-            to="/auth/signup"
-            style={{ color: "#285975", textDecoration: "underline" }}
-          >
-            Sign Up Here
-          </Link>
-        </p>
-        <p style={{ color: "#8E8E93", marginTop: "9px" }}>OR</p>
-        <Button
-          style={{
-            backgroundColor: "#E87070",
             padding: "15px",
-            color: "#ffffff",
+            marginBottom: "20px",
+          }}
+        />
+        {formik.errors.email && formik.touched.email ? (
+          <div className="errorInput">{formik.errors.email}</div>
+        ) : null}
+
+        <input
+          className="loginForm-input"
+          id="password"
+          name="password"
+          type="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          placeholder="Password"
+          style={{
             width: "550px",
             borderRadius: "15px",
-            marginTop: "12px",
+            padding: "15px",
+            marginBottom: "15px",
           }}
-          color="inherit"
-        >
-          Sign In with Google
-        </Button>
-        <p
-          style={{
-            color: "#8E8E93",
-            marginTop: "21px",
-            textAlign: "center",
-            fontSize: "13px",
-            fontWeight: "600",
-          }}
-        >
-          By signing up or signing in, you are agreeing to Levels.fyi's{" "}
-          <span style={{ color: "#285975" }}>Terms of Use</span>
-          <br /> and <span style={{ color: "#285975" }}>Privacy Policy</span>
-        </p>
-      </div>
+        />
+        {formik.errors.password && formik.touched.password ? (
+          <div className="errorInput">{formik.errors.password}</div>
+        ) : null}
+
+        <div className="submitButton">
+          <button
+            style={{
+              width: "550px",
+              backgroundColor: "#163A4E",
+              padding: "15px",
+              color: "#ffffff",
+              borderRadius: "15px",
+              marginTop: "18px",
+              fontSize: "17px",
+              border: "none",
+            }}
+            type="submit"
+          >
+            Sign In
+          </button>
+          <p
+            style={{
+              textAlign: "center",
+              fontSize: "13px",
+              fontWeight: "600",
+              marginTop: "18px",
+            }}
+          >
+            Don’t have an account?{" "}
+            <Link to="/auth/signup">
+              <span
+                style={{
+                  color: "#285975",
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                }}
+              >
+                Sign Up Here
+              </span>
+            </Link>
+          </p>
+          <p
+            style={{
+              color: "#8E8E93",
+              marginTop: "13px",
+              textAlign: "center",
+              fontSize: "15px",
+            }}
+          >
+            OR
+          </p>
+          <button
+            style={{
+              width: "550px",
+              backgroundColor: "#E87070",
+              padding: "15px",
+              color: "#ffffff",
+              borderRadius: "15px",
+              marginTop: "16px",
+              fontSize: "17px",
+              border: "none",
+            }}
+            type="submit"
+          >
+            Sign In with Google
+          </button>
+          <p
+            style={{
+              color: "#8E8E93",
+              marginTop: "21px",
+              textAlign: "center",
+              fontSize: "13px",
+              fontWeight: "600",
+            }}
+          >
+            By signing up or signing in, you are agreeing to Levels.fyi's{" "}
+            <span style={{ color: "#285975" }}>Terms of Use</span>
+            <br /> and <span style={{ color: "#285975" }}>Privacy Policy</span>
+          </p>
+        </div>
+      </form>
     </>
   );
 };
